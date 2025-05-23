db1044a975c46942317265083f0f25e71806c0e5
Ashray Sikka <69213851+AshraySikka@users.noreply.github.com>
2025-05-22 21:14:44 -0400

    Create nested loops.py

---
+++ nested loops.py
@@ -0,0 +1,11 @@
+for i in range(0,5):
+    for j in range(0,5):
+        print('(',i,j,')', end=" ")
+    print('')
+print('\n')
+s1='abc'
+s2='xyz'
+for i in s1:
+    for j in s2:
+        print('(',i,j,')', end=" ")
+    print('')
